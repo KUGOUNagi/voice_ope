54db97364405986e8718aed0f23ccb4ac3ad3b75
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-09-21 17:48:44 +0900

    プロジェクト ファイルを追加します。

---
+++ game_main.cpp
@@ -0,0 +1,18 @@
+#include "DxLib.h"
+
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+
+	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+
+	WaitKey();				// キー入力待ち
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
