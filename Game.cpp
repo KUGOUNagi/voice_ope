aa931eb1e989251ff9a3b2c7df6e0d368aeb9ea4
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-09-30 16:51:39 +0900

    ゲーム画面改良

--- Game.cpp
+++ Game.cpp
@@ -9,7 +9,7 @@
 #define PI 3.1415926f
 
 static int mImageHandle, Handle, don, katt;    //画像ハンドル格納用変数
-int x = 502, hantei = 3, kannkaku = 30, bpm = 5, kaunto, ryou, ka, fuka;
+int y = -5000, hantei = 3, interval = 100, bpm = 30, kaunto, ryou, ka, fuka;
 FILE* fp;
 int a[] = { 0,0,0,1,0,2,0,0,0,1,0,1,0,2,0,0,0,1,0,1,0,
 2,0,1,0,2,0,1,0,2,0,0,0,2,0,1,0,0,0,2,0,2,0,1,0,0,0,1,
@@ -32,25 +32,24 @@ int fumen[] = { 0,0,0,1,0,2,0,0,0,1,0,1,0,2,0,0,0,1,0,
 int fumenn = FileRead_open("fumenn.txt");
 //初期化
 void Game_Initialize() {
-    mImageHandle = LoadGraph("img/taiko.png");    //画像のロード
-    Handle = LoadGraph("img/taiko01.png");
-    don = LoadGraph("img/dontyan01.png");
-    katt = LoadGraph("img/kattyan01.png");
+    mImageHandle = LoadGraph("img/backgraund.png");    //画像のロード
+    don = LoadGraph("img/candy.png");
+    katt = LoadGraph("img/cookie.png");
 }
 
 //終了処理
 void Game_Finalize() {
     DeleteGraph(mImageHandle);    //画像の解放
-    DeleteGraph(Handle);
+    //DeleteGraph(Handle);
     DeleteGraph(don);
     DeleteGraph(katt);
 }
 //更新
 void Game_Update() {
     for (int i = 0; i < 1; i++) {
-        x = x - (kannkaku / bpm);
-        if (CheckHitKey(KEY_INPUT_ESCAPE) != 0) {
-            x = 502;
+        x = x + (interval / bpm);
+        if (CheckHitKey(KEY_INPUT_B) != 0) {
+            x = -5000;
         }
     }//ー側にずらす
     for (int j = 0; j < (sizeof(a) / sizeof(a[0])); j++) {
@@ -66,7 +65,7 @@ void Game_Update() {
             hantei++;
         }
     }
-    if (CheckHitKey(KEY_INPUT_ESCAPE) != 0) {//Escキーが押されていたら
+    if (CheckHitKey(KEY_INPUT_B) != 0) {//Escキーが押されていたら
         for (int i = 0; i < (sizeof(a) / sizeof(a[0])); i++) {
             fumen[i] = a[i];
         }
@@ -75,13 +74,13 @@ void Game_Update() {
     }
     if (Keyboard_Get(KEY_INPUT_G) == 1 || Keyboard_Get(KEY_INPUT_H) == 1) {//G,Hキーが押されていたら
         if (fumen[hantei] == don) {
-            if (197 <= 2 + x + kannkaku * hantei && 2 + x + kannkaku * hantei <= 207) {
+            if (197 <= 2 + x + interval * hantei && 2 + x + interval * hantei <= 207) {
                 ryou++;
             }
-            if (192 <= 2 + x + kannkaku * hantei && 197 > 2 + x + kannkaku * hantei || 2 + x + kannkaku * hantei > 207 && 2 + x + kannkaku * hantei <= 212) {
+            if (192 <= 2 + x + interval * hantei && 197 > 2 + x + interval * hantei || 2 + x + interval * hantei > 207 && 2 + x + interval * hantei <= 212) {
                 ka++;
             }
-            if (192 > 2 + x + kannkaku * hantei || 2 + x + kannkaku * hantei > 212) {
+            if (192 > 2 + x + interval * hantei || 2 + x + interval * hantei > 212) {
                 fuka++;
             }
         }
@@ -94,20 +93,20 @@ void Game_Update() {
     }
     if (Keyboard_Get(KEY_INPUT_F) == 1 || Keyboard_Get(KEY_INPUT_J) == 1) {//F,Jキーが押されていたら
         if (fumen[hantei] == katt) {
-            if (197 <= 2 + x + kannkaku * hantei && 2 + x + kannkaku * hantei <= 207) {
+            if (197 <= 2 + y + interval * hantei && 2 + y + interval * hantei <= 207) {
                 ryou++;
             }
-            if (192 <= 2 + x + kannkaku * hantei && 197 > 2 + x + kannkaku * hantei || 2 + x + kannkaku * hantei > 207 && 2 + x + kannkaku * hantei <= 212) {
+            if (192 <= 2 + y + interval * hantei && 197 > 2 + y + interval * hantei || 2 + y + interval * hantei > 207 && 2 + y + interval * hantei <= 212) {
                 ka++;
             }
-            if (192 > 2 + x + kannkaku * hantei || 2 + x + kannkaku * hantei > 212) {
+            if (192 > 2 + y + interval * hantei || 2 + y + interval * hantei > 212) {
                 fuka++;
             }
         }
         if (fumen[hantei] == don) {
             fuka++;
         }
-        //2 + x + kannkaku * hantei;
+        //2 + x + interval * hantei;
         fumen[hantei] = 0;
         hantei++;
     }
@@ -119,29 +118,25 @@ void Game_Update() {
 
 //描画
 void Game_Draw() {
-    DrawRotaGraph(320, 230, 0.48, 0.0, mImageHandle, FALSE);
-    /*DrawRotaGraph(212, 176, 0.215, 0.0, katt, TRUE);//おおきいカッ
-    DrawRotaGraph(212, 176, 0.14, 0.0, katt, TRUE);//ちいさいカッ
-    DrawRotaGraph(602, 176, 0.17, 0.0, don, TRUE);//ちいさいドン
-    DrawRotaGraph(502, 176, 0.26, 0.0, don, TRUE);//おおきいドン*/
+    DrawRotaGraph(320, 230, 0.9, 0.0, mImageHandle, FALSE);
     for (int i = 0; i < (sizeof(a) / sizeof(a[0])); i++) {
         if (a[i] == 1) {
-            DrawRotaGraph(2 + x + kannkaku * i, 176, 0.17, 0.0, fumen[i], TRUE);//ちいさいドン
+            DrawRotaGraph(176, y + interval * i, 0.7, 0.0, fumen[i], TRUE);//ちいさいドン
         }
         if (a[i] == 2) {
-            DrawRotaGraph(2 + x + kannkaku * i, 176, 0.14, 0.0, fumen[i], TRUE);//ちいさいカッ
+            DrawRotaGraph(176, y + interval * i, 0.7, 0.0, fumen[i], TRUE);//ちいさいカッ
         }
         if (a[i] == 3) {
-            DrawRotaGraph(2 + x + kannkaku * i, 176, 0.26, 0.0, fumen[i], TRUE);//おおきいドン
+            DrawRotaGraph(176, y + interval * i, 0.7, 0.0, fumen[i], TRUE);//おおきいドン
         }
         if (a[i] == 4) {
-            DrawRotaGraph(2 + x + kannkaku * i, 176, 0.215, 0.0, fumen[i], TRUE);//おおきいカッ
+            DrawRotaGraph(176, y + interval * i, 0.7, 0.0, fumen[i], TRUE);//おおきいカッ
         }
     }
-    DrawRotaGraph(82, 185, 0.48, 0.0, Handle, FALSE);
-    DrawString(0, 0, "ゲーム画面です。", GetColor(255, 255, 255));
-    DrawString(0, 20, "Escキーを押すとメニュー画面に戻ります。", GetColor(255, 255, 255));
-    DrawFormatString(0, 40, GetColor(255, 255, 255), "%d", kannkaku);
+    //DrawRotaGraph(82, 185, 1.5, 0.0, Handle, FALSE);
+    //DrawString(0, 0, "ゲーム画面です。", GetColor(255, 255, 255));
+    DrawString(0, 20, "Bキーを押すとメニュー画面に戻ります。", GetColor(255, 255, 255));
+    DrawFormatString(0, 40, GetColor(255, 255, 255), "%d", interval);
     DrawFormatString(300, 0, GetColor(255, 255, 255), "良_%d", ryou);
     DrawFormatString(350, 0, GetColor(255, 255, 255), "可_%d", ka);
     DrawFormatString(400, 0, GetColor(255, 255, 255), "不可_%d", fuka);
