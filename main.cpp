794e9f7e7da06716f4d84775ef39475162fc671e
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-10-11 18:05:03 +0900

    画面サイズ変更

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
     ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
 
-    SetWindowSize(800, 600);
+    SetWindowSize(880, 640);
     while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {//画面更新 & メッセージ処理 & 画面消去
         Keyboard_Update();    //キーボードの更新
         SceneMgr_Update();  //更新
